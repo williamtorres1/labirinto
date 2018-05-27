7ff4ef2ebcb4abe4f00c7ca17b4f1a383b0bcfd8
William Torres <39351781+williamtorres1@users.noreply.github.com>
2018-05-26 23:04:56 -0300

    Update jogo do labirinto.cpp

--- código fonte/jogo do labirinto.cpp
+++ código fonte/jogo do labirinto.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void jogo()//FunÁ„o principal do jogo
+void jogo()//Fun√ß√£o principal do jogo
 {
 	do
 	{
@@ -7,13 +7,13 @@ void jogo()//FunÁ„o principal do jogo
 	}
 	while(1);
 }
-int sorteio_passos()//FunÁ„o para calcular quantos passos poder„o ser realiazdos pelos jogadores
+int sorteio_passos()//Fun√ß√£o para calcular quantos passos poder√£o ser realizados pelos jogadores
 {
 	int passos;
 	return passos;
 }
 void mensagens(int situacao_agora, int player, int passos)
-//situacao_agora == recebera um valor inteiro para determinar qual mensagem ser· mostrada || player == int p/ identificar qual player || quantos passos ser„o dados e quantos restam
+//situacao_agora == recebera um valor inteiro para determinar qual mensagem ser√° mostrada || player == int p/ identificar qual player || quantos passos ser√£o dados e quantos restam
 {
 	switch(situacao_agora)
 	{
@@ -34,7 +34,7 @@ void mensagens(int situacao_agora, int player, int passos)
 			break;
 		}
 		case 5:
-		{	printf("Player %d GANHOU!! PARAB…NS",player);
+		{	printf("Player %d GANHOU!! PARAB√âNS",player);
 			break;
 		}
 	}
@@ -42,7 +42,7 @@ void mensagens(int situacao_agora, int player, int passos)
 char mapa()			//Ainda estou com muitas duvida sobre como  utilizar vetores e matrizes
 {	
 	int linhas=12,colunas=12;
-	char mapa1[linhas][colunas]	// Modelo do labirito a ser percorrido, os caracteres 'X' s„o as paredes que n„o poder„o ser atravessadas
+	char mapa1[linhas][colunas]	// Modelo do labirito a ser percorrido, os caracteres 'X' s√£o as paredes que n√£o poder√£o ser atravessadas
 	{
 	'X','X','X','X','X','X','X','X','X','X','X','X',
 	'X','*','*','*','*','*','*','*','*','*','*','X',
